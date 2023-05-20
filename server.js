2199302ebe1eadf7b716b7608e1ead1e11294e4a
Svitlana Kravets <svetakravez72@gmail.com>
2023-05-20 21:40:22 +0300

    DB connect

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit();
+  });
